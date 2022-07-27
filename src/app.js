157a18507a2690027443a90e997787091c7cb6c4
binhnexle <binhdt@nexle.com>
2022-07-27 22:54:17 +0700

    3 commit

--- src/app.js
+++ src/app.js
@@ -24,16 +24,10 @@ function App() {
           contact={contactSelector}
           languages={languagesSelector}
         />
-<<<<<<< HEAD
         <RightComponents className="app_container_rightComponents" />
         <div>Nhu vay la bi conflick roi ne</div>
-=======
-        <RightComponents className="app_container_rightComponents" />]
-        <div className="app_container">Co y de conflick nè</div>
->>>>>>> 228dea26076916277e0b068b629bf5fb1bbd8abc
-      </div>
- <div className="app_container">conflick lần 2 nè</div>
-
+        <div>Nhu vay la bi conflick lan 2 ne</div>
+        <div>Nhu vay la bi conflick lan 2 ne</div>
       </div>
     </div>
   );
