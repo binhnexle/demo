943d9397af66b68597ffd7ada13868f6ced70427
binhnexle <binhdt@nexle.com>
2022-07-27 22:11:10 +0700

    commit1 on branch1

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ function App() {
         />
         <RightComponents className="app_container_rightComponents" />
       </div>
+      <div className="app_container">Anh dang hoc git do nghe</div>
     </div>
   );
 }
